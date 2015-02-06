f013b48dd65bf374a21aa4e2b00528030f7cc760
ClarkMitchell <clarkcsmitchell@gmail.com>
2015-02-06 11:33:40 -0600

    added background image

--- GamePanel.java
+++ GamePanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.Image;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 
@@ -16,16 +17,17 @@ public class GamePanel extends JPanel{
 	private Controller controller;
 	public float interpolation;
 	BufferStrategy bufferStrategy;
-	
+	Image img;
+
 	int view_xview = 0;
 	int view_yview = 0;
-	
+
 	public GamePanel(Controller c, int x, int y) {
 		super();
 		this.controller = c;
-		
+
 		setPreferredSize(new Dimension(640, 480));
-		
+
 		frame = new JFrame("game");
 		frame.getContentPane().add(this, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,8 +35,11 @@ public class GamePanel extends JPanel{
 		frame.createBufferStrategy(2);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
-		
 		bufferStrategy = frame.getBufferStrategy();
+
+		// to change background, put an image file in the folder and edit this.
+		img = Toolkit.getDefaultToolkit().createImage("grass.jpg");
+
 	}
 
 
@@ -49,32 +54,33 @@ public class GamePanel extends JPanel{
 		Graphics2D g2 = (Graphics2D) bufferStrategy.getDrawGraphics();
 		g2.setPaint(Color.white);
 		g2.fillRect(0, 0, getWidth(), getHeight());
-		
+		g2.drawImage(img, 0, 0, null);
+
 		@SuppressWarnings("unchecked")
 		ArrayList<GameObject> copy = ((ArrayList<GameObject>) controller.list.clone());
 		for(GameObject item : copy)
 		{
 			int drawX = (int) (item.getX() - view_xview);
 			int drawy = (int) (item.getY() - view_yview);
-			
+
 			g2.setColor(item.color);
 			g2.fillOval(drawX, drawy, 32, 32);
-			
+
 			int cX = drawX+16;
 			int cY = drawy+16;
 			g2.setColor(Color.black);
-			g2.drawLine(cX, cY, 
-					cX+(int)ALL.lengthdir_x(16.0, item.getDirection()), 
+			g2.drawLine(cX, cY,
+					cX+(int)ALL.lengthdir_x(16.0, item.getDirection()),
 					cY+(int)ALL.lengthdir_y(16.0, item.getDirection()));
 		}
 		controller.list = copy;
-		
+
 		g2.drawRect(- view_xview, - view_yview, controller.room.width, controller.room.height);
-		
+
 		g2.dispose();
-		
+
 		bufferStrategy.show();
-		
+
 		Toolkit.getDefaultToolkit().sync();
 	}
 }
